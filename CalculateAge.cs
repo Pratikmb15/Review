7aee14012944d70efad9be2ea1215e9f2d5dc2cf
Pratikmb15 <pratikbagul1508@gmail.com>
2025-01-14 10:41:16 +0530

    Add files via upload

---
+++ CalculateAge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review1
+{
+    internal class CalculateAge
+    {
+        public void FindAge(int BirthYear) { 
+        
+        DateTime today = DateTime.Today;
+            if (BirthYear > today.Year)
+            {
+                Console.WriteLine("Enter Correct BirthYear!");
+            }
+            else
+            {
+                int Age =  today.Year -BirthYear ;
+                Console.WriteLine($"Age is {Age}");
+            }
+
+        }
+    }
+}
